fixed normalization and sweep code, BUT curve_toy is broken so something must've caused an issue

scripts/run_model.py
@@ -194,7 +194,7 @@ def run_full_model(config, project, singleton_dataloader, paired_dataloader, tim
     if config.normalize:
         X = singleton_dataloader.dataset.tensors[0]
         mean = torch.mean(X, dim=0, keepdim = True)
-        std = torch.max(torch.std(X, dim=0))
+        std = torch.max(torch.std(X, dim=0)) * np.sqrt(config.pc_dim)
         for model in [classifier_model, metric_model, embed_model, flow_model]:
             model.mean = mean
             model.std = std